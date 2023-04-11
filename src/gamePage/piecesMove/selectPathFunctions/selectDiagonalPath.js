25adafc14fd3f01a6772466dff8a993d8d4da959
MedAmineED <medamineessid5@gmail.com>
2023-04-11 23:01:44 +0100

    fix pieces moves(not completed!!!)

--- src/gamePage/piecesMove/selectPathFunctions/selectDiagonalPath.js
+++ src/gamePage/piecesMove/selectPathFunctions/selectDiagonalPath.js
@@ -1,164 +1,26 @@
-export function selectDiagonalPath(index, piecePos, color, pieceName, playerTurn, updateCases, check) {
-    
+export function selectDiagonalPath(allPossibleMoves, allPiecesData, color, pieceName, position) {
+    const { row, col } = position;
+
     let path1 = true;
     let path2 = true;
     let path3 = true;
     let path4 = true;
 
-    for (let i = 1; i <= 8; i++) {
-        
-                if(piecePos.y  + i < 8 && piecePos.x + i < 8 && path1) {
-                    if(!check){ 
-                            if(updateCases[piecePos.y  + i][piecePos.x + i].empty){
-                                    updateCases[piecePos.y  + i][piecePos.x + i].selected = "tomove" 
-                                    updateCases[piecePos.y  + i][piecePos.x + i].index = index
-                                    updateCases[piecePos.y  + i][piecePos.x + i].color= color 
-                                    updateCases[piecePos.y  + i][piecePos.x + i].pieceName= pieceName
-                            }
-                            if((playerTurn % 2 === 0 && updateCases[piecePos.y + i][piecePos.x + i].eat === "white") 
-                                || (playerTurn % 2 !== 0 && updateCases[piecePos.y + i][piecePos.x + i].eat === "black"))
-                                {
-                                    path1 = false;
-                                    updateCases[piecePos.y  + i][piecePos.x + i].selected = "tomove" 
-                                    updateCases[piecePos.y  + i][piecePos.x + i].index = index
-                                    updateCases[piecePos.y  + i][piecePos.x + i].color= color 
-                                    updateCases[piecePos.y  + i][piecePos.x + i].pieceName= pieceName
-                                }
-                            if((playerTurn % 2 !== 0 && updateCases[piecePos.y + i][piecePos.x + i].eat === "white") 
-                                || (playerTurn % 2 === 0 && updateCases[piecePos.y + i][piecePos.x + i].eat === "black"))
-                                {
-                                   path1 = false   
-                                }
-                    }
-                    if(updateCases[piecePos.y  + i][piecePos.x + i].checked === true){
-                        updateCases[piecePos.y  + i][piecePos.x + i].selected = "tomove" 
-                        updateCases[piecePos.y  + i][piecePos.x + i].index = index
-                        updateCases[piecePos.y  + i][piecePos.x + i].color= color
-                        updateCases[piecePos.y  + i][piecePos.x + i].pieceName= pieceName   
-            
-                    
-                    }
-                    if(updateCases[piecePos.y  + i][piecePos.x + i].checked === false && check && 
-                        (updateCases[piecePos.y  + i][piecePos.x + i].eat === "white"
-                        || updateCases[piecePos.y  + i][piecePos.x + i].eat === "black") 
-                        ) {
-                            path1 = false
-                        } 
-                }
-                if(piecePos.y - i >= 0 && piecePos.x - i >= 0 && path2) {
-                    if(!check){
-                            if(updateCases[piecePos.y  - i][piecePos.x - i].empty){
-                                updateCases[piecePos.y - i][piecePos.x - i].selected = "tomove";
-                                updateCases[piecePos.y - i][piecePos.x - i].index = index;
-                                updateCases[piecePos.y - i][piecePos.x - i].color= color;
-                                updateCases[piecePos.y - i][piecePos.x - i].pieceName= pieceName;
-                            }
-                            if((playerTurn % 2 === 0 && updateCases[piecePos.y - i][piecePos.x - i].eat === "white") 
-                               || (playerTurn % 2 !== 0 && updateCases[piecePos.y - i][piecePos.x - i].eat === "black"))
-                               {
-                                    path2 = false
-                                    updateCases[piecePos.y - i][piecePos.x - i].selected = "tomove";
-                                    updateCases[piecePos.y - i][piecePos.x - i].index = index;
-                                    updateCases[piecePos.y - i][piecePos.x - i].color= color;
-                                    updateCases[piecePos.y - i][piecePos.x - i].pieceName= pieceName;
-                                }
-                            if((playerTurn % 2 !== 0 && updateCases[piecePos.y - i][piecePos.x - i].eat === "white") 
-                               || (playerTurn % 2 === 0 && updateCases[piecePos.y - i][piecePos.x - i].eat === "black"))
-                               {
-                                    path2 = false   
-                               }
-                    }
-                    if(updateCases[piecePos.y - i][piecePos.x - i].checked === true){
-                     
-                        updateCases[piecePos.y - i][piecePos.x - i].selected = "tomove" 
-                        updateCases[piecePos.y - i][piecePos.x - i].index = index
-                        updateCases[piecePos.y - i][piecePos.x - i].color= color
-                        updateCases[piecePos.y - i][piecePos.x - i].pieceName= pieceName
-                    }
-                    if(updateCases[piecePos.y - i][piecePos.x - i].checked === false && check && 
-                        (updateCases[piecePos.y - i][piecePos.x - i].eat === "white"
-                        || updateCases[piecePos.y - i][piecePos.x - i].eat === "black") 
-                        ) {
-                            path2 = false
-                        } 
-                    
-                } 
+        for(let step = 1; step < 8; step ++) {
+          const cellExixst =  row + step  >= 0 
+                                && row + step < 8 
+                                && col + step >= 0 
+                                && col + step < 8? 
+                                true : false;
 
-
-                if(piecePos.y + i < 8 && piecePos.x - i >= 0 && path3) {
-                    if(!check){
-                        if(updateCases[piecePos.y + i][piecePos.x - i].empty){
-                                    updateCases[piecePos.y + i][piecePos.x - i].selected = "tomove" 
-                                    updateCases[piecePos.y + i][piecePos.x - i].index = index 
-                                    updateCases[piecePos.y + i][piecePos.x - i].color= color 
-                                    updateCases[piecePos.y + i][piecePos.x - i].pieceName= pieceName
-                        }
-                        if((playerTurn % 2 === 0 && updateCases[piecePos.y + i][piecePos.x - i].eat === "white")
-                          || (playerTurn % 2 !== 0 && updateCases[piecePos.y + i][piecePos.x - i].eat === "black"))
-                          {
-                                path3 = false
-                                updateCases[piecePos.y + i][piecePos.x - i].selected = "tomove" 
-                                updateCases[piecePos.y + i][piecePos.x - i].index = index 
-                                updateCases[piecePos.y + i][piecePos.x - i].color= color 
-                                updateCases[piecePos.y + i][piecePos.x - i].pieceName= pieceName
-                           }
-                        if((playerTurn % 2 !== 0 && updateCases[piecePos.y + i][piecePos.x - i].eat === "white")
-                          || (playerTurn % 2 === 0 && updateCases[piecePos.y + i][piecePos.x - i].eat === "black"))
-                          {
-                            path3 = false   
-                          }
-                    }
-                    if(updateCases[piecePos.y + i][piecePos.x - i].checked === true){
-                     
-                        updateCases[piecePos.y + i][piecePos.x - i].selected = "tomove" 
-                        updateCases[piecePos.y + i][piecePos.x - i].index = index
-                        updateCases[piecePos.y + i][piecePos.x - i].color= color
-                        updateCases[piecePos.y + i][piecePos.x - i].pieceName= pieceName
-                    }
-                    if(updateCases[piecePos.y + i][piecePos.x - i].checked === false && check && 
-                        (updateCases[piecePos.y + i][piecePos.x - i].eat === "white"
-                        || updateCases[piecePos.y + i][piecePos.x - i].eat === "black") 
-                        ) {
-                            path3 = false
-                        }
-
-                }
-                if(piecePos.y - i >= 0 && piecePos.x + i < 8 && path4) {
-                    if(!check){
-                        if(updateCases[piecePos.y - i][piecePos.x + i].empty){
-                                    updateCases[piecePos.y - i][piecePos.x + i].selected = "tomove" 
-                                    updateCases[piecePos.y - i][piecePos.x + i].index = index 
-                                    updateCases[piecePos.y - i][piecePos.x + i].color= color 
-                                    updateCases[piecePos.y - i][piecePos.x + i].pieceName= pieceName 
-                        }
-                        if((playerTurn % 2 === 0 && updateCases[piecePos.y - i][piecePos.x + i].eat === "white") 
-                            || (playerTurn % 2 !== 0 && updateCases[piecePos.y - i][piecePos.x + i].eat === "black"))
-                            {
-                                path4 = false
-                                updateCases[piecePos.y - i][piecePos.x + i].selected = "tomove" 
-                                updateCases[piecePos.y - i][piecePos.x + i].index = index 
-                                updateCases[piecePos.y - i][piecePos.x + i].color= color 
-                                updateCases[piecePos.y - i][piecePos.x + i].pieceName= pieceName                                                     
-                            }
-                        if((playerTurn % 2 !== 0 && updateCases[piecePos.y - i][piecePos.x + i].eat === "white") 
-                        || (playerTurn % 2 === 0 && updateCases[piecePos.y - i][piecePos.x + i].eat === "black"))
-                            {
-                                path4 = false   
-                            }
-                    }
-                    if(updateCases[piecePos.y - i][piecePos.x + i].checked === true){
-                     
-                        updateCases[piecePos.y - i][piecePos.x + i].selected = "tomove" 
-                        updateCases[piecePos.y - i][piecePos.x + i].index = index
-                        updateCases[piecePos.y - i][piecePos.x + i].color= color
-                        updateCases[piecePos.y - i][piecePos.x + i].pieceName= pieceName
-                    }
-                    if(updateCases[piecePos.y - i][piecePos.x + i].checked === false && check && 
-                        (updateCases[piecePos.y - i][piecePos.x + i].eat === "white"
-                        || updateCases[piecePos.y - i][piecePos.x + i].eat === "black") 
-                        ) {
-                            path4 = false
-                        } 
+          const seLectCells = {
+                            newRow : row + step,
+                            newCol : col + step
+          }
+          if(cellExixst) {
+            allPossibleMoves.push({ row : seLectCells.newRow, col : seLectCells.newCol })
+          }
         }
-    }
+    
+    
 }
--- src/gamePage/piecesMove/selectPathFunctions/selectPath.js
+++ src/gamePage/piecesMove/selectPathFunctions/selectPath.js
@@ -1,7 +1,9 @@
+import { selectDiagonalPath } from "./selectDiagonalPath";
 import { selectPawnPath } from "./selectPawnPath";
 import { selectRookPath } from "./selectRookPath";
 
 export function selectPath (allPossibleMoves, allPiecesData, color, pieceName, position) {
     if(pieceName === "pawn")selectPawnPath(...arguments)
     if(pieceName === "rook" || pieceName === "queen")selectRookPath(...arguments)
+    if(pieceName === "bishop" || pieceName === "queen")selectDiagonalPath(...arguments)
 }
