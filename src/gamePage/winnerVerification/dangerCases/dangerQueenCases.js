0eb08cf9dc4c9f57407fd9c665ea63616e615c64
MedAmineED <medamineessid5@gmail.com>
2023-04-14 02:41:21 +0100

    fix under arttack cases(not completed!!!)

--- src/gamePage/winnerVerification/dangerCases/dangerQueenCases.js
+++
@@ -1,7 +0,0 @@
-// import { diagonalDangerZone } from "./dangerBishopCases";
-// import { dangerRookZone } from "./dangerRookCases";
-
-// export function dangerQueenZone(y, x, board, updateCases) {
-//     dangerRookZone(...arguments)
-//     diagonalDangerZone(...arguments)
-// }
